549018e677bb99e0a8e77efc29b4e7e8348b91d7
Thalisson-Alves <thalisson2030@gmail.com>
2020-08-09 04:32:09 -0300

    Update max_heap.h

--- max_heap/max_heap.h
+++ max_heap/max_heap.h
@@ -1,45 +1,58 @@
+#include <stdlib.h>
 #include <stdbool.h>
 
 #ifndef __MAX_HEAP_H__
 #define __MAX_HEAP_H__
 
-const int kMaxCapacity = 1000;
+/* constants for dinamic allocation */
+const int kMinCapacity = 16;
+const int kShirinkRate = 4;
+const int kGrowthRate = 2;
 
 typedef struct max_heap
 {
-	// index of elements starts with 1
-	int elements[1000];
+	int *elements;
 	int size;
 	int capacity;
 } max_heap;
 
 /* PROTOTYPE */
 
+// initialize the values, allocating memory for elements
 max_heap max_heap_new();
-
-void max_heap_push(max_heap *heap, int value);
-
+// deallocate memory
+void max_heap_destroy(max_heap *heap);
+// add element to max heap
+void max_heap_insert(max_heap *heap, int value);
+// restore the heap propery
+// propagating the given node index up up the tree
 void max_heap_sift_up(max_heap *heap, int index);
-
-int max_heap_top(max_heap heap);
-
-int max_heap_size(max_heap heap);
-
-bool max_heap_empty(max_heap heap);
-
-int max_heap_pop(max_heap *heap);
-
+// return the max value of heap, -1 if empty
+int max_heap_get_max(max_heap heap);
+// return size of heap
+int max_heap_get_size(max_heap heap);
+// return true if heap id empty
+bool max_heap_get_is_empty(max_heap heap);
+// return the max value of heap, -1 if empty
+int max_heap_extract_max(max_heap *heap);
+// restore the heap propery
+// propagating the given node index down up the tree
 void max_heap_sift_down(max_heap *heap, int index);
-
+// remove element of a given index
 void max_heap_remove(max_heap *heap, int index);
-
+// same of sift_down but for 0 indexes array
 void heapify(int *arr, int size, int index);
-
+// sorts a given array to satisfy the heap propery
 void build_max_heap(int arr[], int size);
-
+// sorts a given array in non-decreasing order
 void heap_sort(int *arr, int size);
-
+// swap 2 elements
 void swap_elements(int *a, int *b);
+// resize the elements array to the given size
+// allocating memory if needed
+void max_heap_resize_for_size(max_heap *heap, int new_size);
+// check address of a pointer exit the program if it's null
+void check_address(void *ptr);
 
 
 /* IMPLEMENTATION */
@@ -48,17 +61,20 @@ max_heap max_heap_new()
 {
 	max_heap heap;
 	heap.size = 0;
-	heap.capacity = kMaxCapacity;
+	heap.capacity = kMinCapacity;
+	heap.elements = malloc(sizeof(int) * heap.capacity);
 	
 	return heap;
 }
 
-void max_heap_push(max_heap *heap, int value)
+void max_heap_destroy(max_heap *heap)
 {
-	if (heap->size == kMaxCapacity) {
-		// error
-		return;
-	}
+	free(heap->elements);
+}
+
+void max_heap_insert(max_heap *heap, int value)
+{
+	max_heap_resize_for_size(heap, heap->size + 1);
 	heap->elements[++heap->size] = value;
 	max_heap_sift_up(heap, heap->size);
 }
@@ -74,28 +90,28 @@ void max_heap_sift_up(max_heap *heap, int index)
 	max_heap_sift_up(heap, parent);
 }
 
-int max_heap_top(max_heap heap)
+int max_heap_get_max(max_heap heap)
 {
 	return heap.size < 1? -1: heap.elements[1];
 }
 
-int max_heap_size(max_heap heap)
+int max_heap_get_size(max_heap heap)
 {
 	return heap.size;
 }
 
-bool max_heap_empty(max_heap heap)
+bool max_heap_get_is_empty(max_heap heap)
 {
 	return heap.size == 0;
 }
 
-int max_heap_pop(max_heap *heap)
+int max_heap_extract_max(max_heap *heap)
 {
-	if (max_heap_empty(*heap)) {
-		// error
-		return 0;
+	if (max_heap_get_is_empty(*heap)) {
+		return -1;
 	}
-	int top = max_heap_top(*heap);
+	max_heap_resize_for_size(heap, heap->size - 1);
+	int top = max_heap_get_max(*heap);
 	heap->elements[1] = heap->elements[heap->size--];
 	max_heap_sift_down(heap, 1);
 	return top;
@@ -131,8 +147,10 @@ void max_heap_sift_down(max_heap *heap, int index)
 
 void max_heap_remove(max_heap *heap, int index)
 {
-	if (index > heap->size)
-		return;
+	if (index > heap->size || (index < 1 && heap->size)) {
+		return ;
+	}
+	max_heap_resize_for_size(heap, heap->size - 1);
 	heap->elements[index] = heap->elements[heap->size--];
 	max_heap_sift_down(heap, index);
 }
@@ -191,10 +209,49 @@ void swap_elements(int *a, int *b)
 	*b = tmp;
 }
 
+void max_heap_resize_for_size(max_heap *heap, int new_size)
+{
+	if (new_size > heap->size) {
+		if (new_size > heap->capacity) {
+			// grow tree
+			int new_capacity = heap->capacity * kGrowthRate;
+			int *new_elements = realloc(heap->elements, sizeof(int) * new_capacity);
+			check_address(new_elements);
+			heap->elements = new_elements;
+			heap->capacity = new_capacity;
+		}
+	}
+	else {
+		if (heap->capacity / kShirinkRate > new_size) {
+			int new_capacity = heap->capacity / kShirinkRate;
+			if (new_capacity < kMinCapacity) {
+				new_capacity = kMinCapacity;
+			}
+			// Do nothing
+			if (new_capacity == heap->capacity) {
+				return ;
+			}
+			int *new_elements = realloc(heap->elements, sizeof(int) * new_capacity);
+			check_address(new_elements);
+			heap->elements = new_elements;
+			heap->capacity = new_capacity;
+		}
+	}
+}
+
+void check_address(void *ptr)
+{
+	if (!ptr) {
+		printf("Bad allocation\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
 void debug(max_heap heap)
 {
 	printf("Size: %i\n", heap.size);
-	printf("Max element: %i\n", max_heap_top(heap));
+	printf("Capacity: %i\n", heap.capacity);
+	printf("Max element: %i\n", max_heap_get_max(heap));
 	for (int i = 1; i <= heap.size; i++)
 		printf("%i ", heap.elements[i]);
 	printf("\n-----------------\n");
